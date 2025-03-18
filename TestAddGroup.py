6ceb674a2ba719b22e4fc7c33393113b7e2e5f54
zoro <scwinck@gmail.com>
2025-03-18 12:06:31 +0600

    add application.py and allocated fixture

--- TestAddGroup.py
+++ TestAddGroup.py
@@ -1,46 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
-
+import pytest
 from application import Application
 from group import Group
+#Файл, где при помощи вспомогательных функций делают тесты
+@pytest.fixture #Инициализация фикстуры
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy) #Указание на то, как эта фикстура должна разрушаться
+    return fixture
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+def test_add_group(app): #Тестовый метод
+    app.login("admin", "secret")
+    app.create_group(Group("House", "asl", "da"))
+    app.logout()
 
-    def test_add_group(self):
-        self.app.login("admin", "secret")
-        self.app.create_group(Group("House", "asl", "da"))
-        self.app.logout()
+def test_add_empty_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("", "", ""))
+    app.logout()
 
-    def test_add_empty_group(self):
-        self.app.login("admin", "secret")
-        self.app.create_group(Group("", "", ""))
-        self.app.logout()
-
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to.alert
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
--- application.py
+++ application.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-
+#Сами вспомогательные функции
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
